71b2c506ecb36bd592d2f5006a257d02e76f412f
Krzesimir Hyżyk <krzem5.dev@gmail.com>
2022-10-23 22:14:09 +0200

    Push Update 23/10/2022, 22:14:09

--- src/frobenius_number_calculator/frobenius_number_calculator.c
+++ src/frobenius_number_calculator/frobenius_number_calculator.c
@@ -1,5 +1,21 @@
+#ifdef __SLL_BUILD_WINDOWS
+#include <intrin.h>
+#endif
 #include <stdint.h>
 #include <stdlib.h>
+
+
+
+#ifdef _MSC_VER
+#pragma intrinsic(_BitScanReverse64)
+static __SLL_FORCE_INLINE uint32_t FIND_LAST_SET_BIT(uint64_t m){
+	unsigned long o;
+	_BitScanReverse64(&o,m);
+	return o;
+}
+#else
+#define FIND_LAST_SET_BIT(m) (63-__builtin_clzll((m)))
+#endif
 
 
 
@@ -13,26 +29,94 @@ uint64_t frobenius_number(const uint64_t* data,uint64_t count){
 	if (count==2){
 		return (data[0]-1)*(data[1]-1)-1;
 	}
-	uint64_t a1=data[0];
+	uint64_t* a=malloc((2*count-1)*sizeof(uint64_t));
+	uint64_t* b=a+count;
+	for (uint64_t i=0;i<count;i++){
+		a[i]=data[i];
+	}
+	uint64_t merge_sort_tmp_length=1<<(FIND_LAST_SET_BIT(count)+1);
+	if (merge_sort_tmp_length>count){
+		merge_sort_tmp_length=count;
+	}
+	uint64_t* left=malloc(2*merge_sort_tmp_length*sizeof(uint64_t));
+	uint64_t* right=left+merge_sort_tmp_length;
+	uint64_t w=1;
 	count--;
-	data++;
+	while (w<=count){
+		uint64_t l=0;
+		while (l<=count){
+			uint64_t m=l+w-1;
+			uint64_t r=m+w;
+			if (m>count){
+				m=count;
+			}
+			if (r>count){
+				r=count;
+			}
+			uint64_t left_l=m-l+1;
+			uint64_t right_l=r-m;
+			if (!right_l){
+				l+=w<<1;
+				continue;
+			}
+			for (uint64_t i=0;i<left_l;i++){
+				left[i]=a[l+i];
+			}
+			for (uint64_t i=0;i<right_l;i++){
+				right[i]=a[m+i+1];
+			}
+			uint64_t i=0;
+			uint64_t j=0;
+			while (1){
+				if (left[i]>right[j]){
+					a[l]=right[j];
+					j++;
+					if (j==right_l){
+						do{
+							l++;
+							a[l]=left[i];
+							i++;
+						} while (i<left_l);
+						break;
+					}
+				}
+				else{
+					a[l]=left[i];
+					i++;
+					if (i==left_l){
+						do{
+							l++;
+							a[l]=right[j];
+							j++;
+						} while (j<right_l);
+						break;
+					}
+				}
+				l++;
+			}
+			l++;
+		}
+		w<<=1;
+	}
+	uint64_t a1=a[0];
+	a++;
 	uint64_t l=(a1+63)>>6;
-	uint64_t* b=malloc((count+l+3*a1)*sizeof(uint64_t));
-	uint64_t* m=b+count;
+	uint64_t* m=malloc((l+3*a1)*sizeof(uint64_t));
 	uint64_t* p=m+l;
 	uint64_t* q=p+a1;
 	uint64_t* s=q+a1;
+	free(left);
 	count--;
 	p[0]=count;
 	q[0]=0;
 	s[0]=0;
 	for (uint64_t i=0;i<=count;i++){
-		b[i]=data[i]%a1;
+		b[i]=a[i]%a1;
 	}
 	for (uint64_t i=0;i<l;i++){
 		m[i]=0;
 	}
-	uint64_t v=a1*data[count];
+	uint64_t v=a1*a[count];
 	for (uint64_t i=1;i<a1;i++){
 		s[i]=v;
 	}
@@ -51,7 +135,7 @@ uint64_t frobenius_number(const uint64_t* data,uint64_t count){
 			if (v>=a1){
 				v-=a1;
 			}
-			uint64_t w=t+data[k];
+			uint64_t w=t+a[k];
 			if (w>=s[v]){
 				continue;
 			}
@@ -69,12 +153,13 @@ uint64_t frobenius_number(const uint64_t* data,uint64_t count){
 			m[v>>6]|=n;
 		}
 	} while (i!=j);
+	free(a-1);
 	uint64_t out=0;
 	for (uint32_t i=0;i<a1;i++){
 		if (s[i]>out){
 			out=s[i];
 		}
 	}
-	free(b);
+	free(m);
 	return out-a1;
 }
