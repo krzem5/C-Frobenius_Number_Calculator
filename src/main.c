83222eb859ce9090c8fc53a2953b7918820bbb40
Krzesimir Hyżyk <krzem5.dev@gmail.com>
2022-10-23 22:16:34 +0200

    Push Update 23/10/2022, 22:16:34

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 
 int main(int argc,const char** argv){
 	uint64_t nums_a[]={5,7};
-	uint64_t nums_b[]={6,9,20};
+	uint64_t nums_b[]={9,6,20};
 	uint64_t nums_c[]={999991,999992,999993,999994,999995,999996,999997,999998,999999,1000000};
 	printf("%lu\n",frobenius_number(nums_a,sizeof(nums_a)/sizeof(uint64_t)));
 	printf("%lu\n",frobenius_number(nums_b,sizeof(nums_b)/sizeof(uint64_t)));
