751fbbab1e98b49a1bc70a823963f702ac369d39
Krzesimir Hyżyk <krzem5.dev@gmail.com>
2022-10-23 20:39:37 +0200

    Push Update 23/10/2022, 20:39:37

--- src/frobenius_number_calculator/frobenius_number_calculator.c
+++ src/frobenius_number_calculator/frobenius_number_calculator.c
@@ -19,16 +19,17 @@ uint64_t frobenius_number(const uint64_t* data,uint64_t count){
 	uint64_t* p=m+l;
 	uint64_t* q=p+a1;
 	uint64_t* s=q+a1;
+	count--;
 	p[0]=count;
 	q[0]=0;
 	s[0]=0;
-	for (uint64_t i=0;i<count;i++){
+	for (uint64_t i=0;i<=count;i++){
 		b[i]=data[i]%a1;
 	}
 	for (uint64_t i=0;i<l;i++){
 		m[i]=0;
 	}
-	uint64_t v=a1*data[count-1];
+	uint64_t v=a1*data[count];
 	for (uint64_t i=1;i<a1;i++){
 		s[i]=v;
 	}
@@ -42,7 +43,7 @@ uint64_t frobenius_number(const uint64_t* data,uint64_t count){
 		}
 		m[u>>6]&=~(1ull<<(u&63));
 		uint64_t t=s[u];
-		for (uint64_t k=0;k<p[u];k++){
+		for (uint64_t k=0;k<=p[u];k++){
 			uint64_t v=u+b[k];
 			if (v>=a1){
 				v-=a1;
@@ -52,7 +53,7 @@ uint64_t frobenius_number(const uint64_t* data,uint64_t count){
 				continue;
 			}
 			s[v]=w;
-			p[v]=k+1;
+			p[v]=k;
 			uint64_t n=1ull<<(v&63);
 			if (m[v>>6]&n){
 				continue;
